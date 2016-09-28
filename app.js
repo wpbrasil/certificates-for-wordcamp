67f3279e5339b63d1f1f3ee6d740614edbba7822
Claudio Sanches <contato@claudiosmweb.com>
2016-09-28 14:12:04 -0300

    Initial form

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 'use strict';
 
+var bodyParser      = require('body-parser');
 var config          = require('config-yml');
+var cookieParser    = require('cookie-parser');
+var csrf            = require('csurf');
 var express         = require('express');
 var expressPDF      = require('express-pdf');
 var fs              = require('fs');
@@ -11,6 +14,10 @@ var path            = require('path');
 // Setup app
 var app = express();
 
+// Setup route middlewares
+var csrfProtection = csrf({ cookie: true })
+var parseForm = bodyParser.urlencoded({ extended: false })
+
 // Setup Mustache in Express
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
@@ -19,15 +26,20 @@ app.set('views', __dirname + '/views');
 // Set Express PDF
 app.use(expressPDF);
 
+// Parse cookies
+app.use(cookieParser());
+
 // Routes
-app.get('/', function (req, res) {
-  res.render('index', {
-    title: config.site.title,
-    message: config.site.message
-  });
+app.get('/', csrfProtection, function (req, res) {
+  var args        = config.site.index;
+  args.lang       = config.site.lang;
+  args.csrfToken  = req.csrfToken();
+  args.formAction = config.routes.certificate;
+
+  res.render('index', args);
 });
 
-app.use('/certificate', function (req, res) {
+app.post('/' + config.routes.certificate, parseForm, csrfProtection, function (req, res) {
   var templatePath = path.resolve(__dirname, './views/pdf.html');
   var args = {};
 
@@ -37,12 +49,22 @@ app.use('/certificate', function (req, res) {
     }
 
     res.pdfFromHTML({
-      filename: 'generated.pdf',
+      filename: 'certificate.pdf',
       htmlContent: mustache.render(data.toString(), args)
       // options: {...}
     });
   });
 });
+
+app.use(function (err, req, res, next) {
+  if (err.code !== 'EBADCSRFTOKEN') {
+    return next(err);
+  }
+
+  // Handle CSRF token errors here
+  res.status(403);
+  res.send('ERROR MESSAGE HERE');
+})
 
 app.listen(3000, function () {
   console.log('Site running on port 3000!');
